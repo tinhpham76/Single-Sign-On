a1e28150ef2de2f656cea991584058881620eaf5
Tịnh Phạm <tinh_pham@outlook.com>
2020-07-07 11:48:40 +0700

    update

--- src/Frontend/admin-app/src/environments/environment.prod.ts
+++ src/Frontend/admin-app/src/environments/environment.prod.ts
@@ -1,4 +1,10 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://kbapi.tedu.com.vn'
+  production: false,
+  apiUrl: 'https://localhost:5000',
+  authority: 'https://localhost:5000',
+  client_id: 'angular_admin',
+  redirect_uri: 'http://localhost:4200/auth-callback',
+  post_logout_redirect_uri: 'http://localhost:4200/',
+  scope: 'SSO_API openid profile',
+  silent_redirect_uri: 'http://localhost:4200/silent-refresh.html'
 };
--- src/Frontend/profile-app/src/environments/environment.prod.ts
+++ src/Frontend/profile-app/src/environments/environment.prod.ts
@@ -1,4 +1,10 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://kbapi.tedu.com.vn'
+  production: false,
+  apiUrl: 'https://localhost:5001',
+  authority: 'https://localhost:5000',
+  client_id: 'angular_user_manager',
+  redirect_uri: 'http://localhost:4300/auth-callback',
+  post_logout_redirect_uri: 'http://localhost:4300/',
+  scope: 'USER_API SSO_API openid profile',
+  silent_redirect_uri: 'http://localhost:4300/silent-refresh.html'
 };
